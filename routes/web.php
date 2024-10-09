299a6d53c89ce2e0d466a2b20748863bf7041c85
nadheakurniaramadhani <nanadkurniars@gmail.com>
2024-10-09 14:22:53 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -37,94 +37,69 @@ Route::middleware(['guest'])->group(function(){
     return view('kelas');
 });
 });
-Route::middleware(['auth'])->group(function(){
+//Hak  akses user
+    Route::middleware(['auth'])->group(function(){
     Route::get('/user',[AdminController::class,'index'])->middleware('userAkses:user');
+    Route::get('/user/kelas',[DataKelasController::class,'show'])->middleware('userAkses:user');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile')->middleware('userAkses:user');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit')->middleware('userAkses:user');
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update')->middleware('userAkses:user');
+    Route::get('/user/{id}/payment/', [PembelianController::class, 'index'])->name('user.payment')->middleware('userAkses:user');
+    Route::get('/user/kelas/materi',[KelasMateriController::class,'index'])->name('kelas.materi')->middleware('userAkses:user');
+    Route::get('/user/{id}/materi', [KelasMateriController::class, 'materi'])->name('materi.user')->middleware('userAkses:user');
+    Route::get('/user/{id}/submateri',[KelasMateriController::class,'submateri'])->name('submateri.user')->middleware('userAkses:user');
+    Route::get('/user/{id}/belajar/', [KelasMateriController::class, 'belajar'])->name('belajar.user')->middleware('userAkses:user');
+    Route::get('/bukti/{id}/pembayaran',[PembelianController::class,'pembayaran'])->name('bukti.pembayaran')->middleware('userAkses:user');
+    Route::post('/kirim/bukti', [PembelianController::class, 'BuktiPembayaran'])->name('kirim.bukti')->middleware('userAkses:user');
+
+//Hak akses trainer
     Route::get('/trainer',[AdminController::class,'trainer'])->middleware('userAkses:trainer');
+    Route::get('/profiletrainer', [ProfileTrainerController::class, 'index'])->name('profiletrainer')->middleware('userAkses:trainer');
+    Route::get('/profiletrainer/edit', [ProfileTrainerController::class, 'edit'])->name('profiletrainer.edit')->middleware('userAkses:trainer');
+    Route::put('/profiletrainer/update', [ProfileTrainerController::class, 'update'])->name('profiletrainer.update')->middleware('userAkses:trainer');
+    Route::get('Trainer/TambahMateri',[MateriController::class,'Trainer'])->name('Trainer.TambahMateri')->middleware('userAkses:trainer');
+    Route::get('trainer/tambahmateri/search', [MateriController::class, 'searchkelas'])->name('trainer.tambahmateri.search')->middleware('userAkses:trainer');
+    Route::get('/trainer.create.materi', [MateriController::class, 'index'])->name('trainer.create.materi')->middleware('userAkses:trainer');
+    Route::post('/materi/create', [MateriController::class, 'create'])->name('materi.create')->middleware('userAkses:trainer');
+    Route::get('/kelas/{kelas}/materi', [MateriController::class, 'materi'])->name('materi')->middleware('userAkses:trainer');
+    Route::get('/materi/{id}/edit', [MateriController::class, 'edit'])->name('materi.edit')->middleware('userAkses:trainer');
+    Route::put('/materi/{id}/update', [MateriController::class, 'update'])->name('materi.update')->middleware('userAkses:trainer');
+    Route::delete('materi/{id}/destroy', [MateriController::class, 'destroy'])->name('materi.destroy')->middleware('userAkses:trainer');
+    Route::delete('submateri/{id}/destroy', [MateriController::class, 'destroysub'])->name('submateri.destroy')->middleware('userAkses:trainer');
+    Route::get('/materi/{materi}/submateri',[ SubmateriController::class, 'show'])->name('materi.submateri')->middleware('userAkses:trainer');
+    Route::get('/tambah/submateri',[SubmateriController::class,'index'])->name('tambah.submateri')->middleware('userAkses:trainer');
+    Route::post('/tambah/submateri',[SubmateriController::class,'create'])->name('create.submateri')->middleware('userAkses:trainer');
+
+//Hak akses admin
     Route::get('/admin',[AdminController::class,'admin'])->middleware('userAkses:admin');
+    Route::get('/create/kelas',[DataKelasController::class, 'kelas'])->middleware('userAkses:admin');
+    Route::post('/kelas/create', [DataKelasController::class, 'create'])->middleware('userAkses:admin');
+    Route::get('/datakursus/{id}/edit', [DataKelasController::class, 'editkursus'])->name('edit.datakursus')->middleware('userAkses:admin');
+    Route::put('/kursus/{id}update', [DataKelasController::class, 'updatekursus'])->name('kursus.update')->middleware('userAkses:admin');
+    Route::delete('/kursus/{id}destroy', [DataKelasController::class, 'destroykursus'])->name('kursus.destroy')->middleware('userAkses:admin');
+    Route::get('/admin/Data-kelas/search', [DataKelasController::class, 'search'])->name('admin.data-kelas.search')->middleware('userAkses:admin');
+    Route::get('/admin/DataKelas',[DataKelasController::class,'index'])->name('datakelas')->middleware('userAkses:admin');
+    Route::get('/admin/DataUser', [UserController::class, 'index'])->name('admin.DataUser')->middleware('userAkses:admin');
+    Route::get('/admin/data-user/{user}/edit', [UserController::class, 'edituser'])->name('admin.dataUser.edit')->middleware('userAkses:admin');
+    Route::put('/admin/data-user/{user}', [UserController::class, 'updateuser'])->name('admin.dataUser.update')->middleware('userAkses:admin');
+    Route::delete('/admin/data-user/{user}', [UserController::class, 'destroy'])->name('admin.DataUser.destroy')->middleware('userAkses:admin');
+    Route::get('admin/data-user/search', [UserController::class, 'index'])->name('admin.dataUser.search')->middleware('userAkses:admin');
+    Route::get('/admin/data-trainer/{user}/edit', [UserController::class, 'edittrainer'])->name('admin.dataTrainer.edit')->middleware('userAkses:admin');
+    Route::put('/admin/data-trainer/{user}', [UserController::class, 'updatetrainer'])->name('admin.dataTrainer.update')->middleware('userAkses:admin');
+    Route::get('/admin/Data-trainer', [UserController::class, 'trainer'])->name('admin.dataTrainer')->middleware('userAkses:admin'); 
+    Route::get('create/trainer', [RegisterController::class, 'registertrainer'])->name('create/trainer')->middleware('userAkses:admin');
+    Route::post('/register/create', [RegisterController::class, 'tambah'])->name('register.trainer')->middleware('userAkses:admin');
+    Route::delete('/admin/data-trainer/{trainer}', [UserController::class, 'destroytrainer'])->name('admin.DataTrainer.destroy')->middleware('userAkses:admin');
+    Route::get('/admin/data-trainer/search', [UserController::class, 'searchtrainer'])->name('admin.data-trainer.search')->middleware('userAkses:admin');
+    Route::get('/admin/data-pembelian', [PembelianController::class, 'datapembelian'])->name('admin.DataPembelian')->middleware('userAkses:admin');
+    Route::get('/admin/add-trainer', [DataKelasController::class, 'AddTrainerForm'])
+    ->name('FormAddTrainer')->middleware('userAkses:admin');
+    Route::post('/admin/add-trainer-to-class', [DataKelasController::class, 'addTrainerToClass'])
+    ->name('addTrainerToClass')->middleware('userAkses:admin');
+    Route::get('/admin/{kelas}/materi', [MateriController::class, 'materiadmin'])->name('lihat.materi')->middleware('userAkses:admin');
+    Route::post('/pembayaran/approve/{id}', [PembelianController::class, 'approve'])->name('pembayaran.approve')->middleware('userAkses:admin');
+    Route::post('/pembayaran/reject/{id}', [PembelianController::class, 'reject'])->name('pembayaran.reject')->middleware('userAkses:admin');
+    Route::get('admin/materi/{apaaja}/submateri',[ SubmateriController::class, 'submateri_admin'])->name('admin.materi.submateri')->middleware('userAkses:admin');
+
     Route::get('/logout', [LoginController::class,'logout'])->name('logout');
 });
-
-// Route untuk Data kursus
-Route::get('/admin/DataKelas',[DataKelasController::class,'index'])->name('datakelas');
-Route::get('/user/kelas',[DataKelasController::class,'show']);
-Route::get('/create/kelas',[DataKelasController::class, 'kelas']);
-Route::post('/kelas/create', [DataKelasController::class, 'create']);
-Route::get('/datakursus/{id}/edit', [DataKelasController::class, 'editkursus'])->name('edit.datakursus');
-Route::put('/kursus/{id}update', [DataKelasController::class, 'updatekursus'])->name('kursus.update');
-Route::delete('/kursus/{id}destroy', [DataKelasController::class, 'destroykursus'])->name('kursus.destroy');
-Route::get('/admin/Data-kelas/search', [DataKelasController::class, 'search'])->name('admin.data-kelas.search');
-
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
-Route::get('/profiletrainer', [ProfileTrainerController::class, 'index'])->name('profiletrainer');
-Route::get('/profiletrainer/edit', [ProfileTrainerController::class, 'edit'])->name('profiletrainer.edit');
-Route::put('/profiletrainer/update', [ProfileTrainerController::class, 'update'])->name('profiletrainer.update');
-
-// Route untuk data user
-Route::get('/admin/DataUser', [UserController::class, 'index'])->name('admin.DataUser');
-Route::get('/admin/data-user/{user}/edit', [UserController::class, 'edituser'])->name('admin.dataUser.edit');
-Route::put('/admin/data-user/{user}', [UserController::class, 'updateuser'])->name('admin.dataUser.update');
-Route::delete('/admin/data-user/{user}', [UserController::class, 'destroy'])->name('admin.DataUser.destroy');
-Route::get('admin/data-user/search', [UserController::class, 'index'])->name('admin.dataUser.search');
-
-
-Route::get('Trainer/TambahMateri',[MateriController::class,'Trainer'])->name('Trainer.TambahMateri');
-Route::get('trainer/tambahmateri/search', [MateriController::class, 'searchkelas'])->name('trainer.tambahmateri.search');
-
-// Route untuk DataTrainer
-Route::get('/admin/data-trainer/{user}/edit', [UserController::class, 'edittrainer'])->name('admin.dataTrainer.edit');
-Route::put('/admin/data-trainer/{user}', [UserController::class, 'updatetrainer'])->name('admin.dataTrainer.update');
-Route::get('/admin/Data-trainer', [UserController::class, 'trainer'])->name('admin.dataTrainer'); 
-Route::get('create/trainer', [RegisterController::class, 'registertrainer'])->name('create/trainer');
-Route::post('/register/create', [RegisterController::class, 'tambah'])->name('register.trainer');
-Route::delete('/admin/data-trainer/{trainer}', [UserController::class, 'destroytrainer'])->name('admin.DataTrainer.destroy');
-Route::get('/admin/data-trainer/search', [UserController::class, 'searchtrainer'])->name('admin.data-trainer.search');
-
-//Route untuk data pembelian
-Route::get('/admin/data-pembelian', [PembelianController::class, 'datapembelian'])->name('admin.DataPembelian');
-
-Route::get('/trainer.create.materi', [MateriController::class, 'index'])->name('trainer.create.materi');
-Route::post('/materi/create', [MateriController::class, 'create'])->name('materi.create');
-
-
-Route::get('/kelas/{kelas}/materi', [MateriController::class, 'materi'])->name('materi');
-Route::get('/materi/{id}/edit', [MateriController::class, 'edit'])->name('materi.edit');
-Route::put('/materi/{id}/update', [MateriController::class, 'update'])->name('materi.update');
-Route::delete('materi/{id}/destroy', [MateriController::class, 'destroy'])->name('materi.destroy');
-Route::delete('submateri/{id}/destroy', [MateriController::class, 'destroysub'])->name('submateri.destroy');
-Route::get('/submateri/{id}/edit', [MateriController::class, 'editsub'])->name('submateri.edit');
-Route::put('/submateri/{id}/update', [MateriController::class, 'updatesub'])->name('submateri.update');
-
-
-Route::get('/admin/add-trainer', [DataKelasController::class, 'AddTrainerForm'])
-    ->name('FormAddTrainer');
-Route::post('/admin/add-trainer-to-class', [DataKelasController::class, 'addTrainerToClass'])
-    ->name('addTrainerToClass');
-
-Route::get('/user.payment', function () {
-    return view('/user/payment');
-});
-Route::get('/user/{id}/payment/', [PembelianController::class, 'index'])->name('user.payment');
-
-Route::get('/admin/{kelas}/materi', [MateriController::class, 'materiadmin'])->name('lihat.materi');
-
-Route::get('/user/kelas/materi',[KelasMateriController::class,'index'])->name('kelas.materi');
-Route::get('/user/{id}/materi', [KelasMateriController::class, 'materi'])->name('materi.user');
-Route::get('/user/{id}/submateri',[KelasMateriController::class,'submateri'])->name('submateri.user');
-Route::get('/user/{id}/belajar/', [KelasMateriController::class, 'belajar'])->name('belajar.user');
-
-Route::get('/tambah/submateri',[SubmateriController::class,'index'])->name('tambah.submateri');
-Route::post('/tambah/submateri',[SubmateriController::class,'create'])->name('create.submateri');
-
-Route::get('/materi/{materi}/submateri',[ SubmateriController::class, 'show'])->name('materi.submateri');
-Route::get('admin/materi/{apaaja}/submateri',[ SubmateriController::class, 'submateri_admin'])->name('admin.materi.submateri');
-
-Route::get('/bukti/{id}/pembayaran',[PembelianController::class,'pembayaran'])->name('bukti.pembayaran');
-Route::post('/kirim/bukti', [PembelianController::class, 'BuktiPembayaran'])->name('kirim.bukti');
-
-Route::post('/pembayaran/approve/{id}', [PembelianController::class, 'approve'])->name('pembayaran.approve');
-Route::post('/pembayaran/reject/{id}', [PembelianController::class, 'reject'])->name('pembayaran.reject');
-
-
