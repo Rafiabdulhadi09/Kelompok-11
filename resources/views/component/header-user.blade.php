ccee377d947b3f0b063174e6d39404855ccf0e53
R09AH <rafiabdulh09@gmail.com>
2024-10-17 10:59:53 +0700

    atos diubah

--- resources/views/component/header-user.blade.php
+++ resources/views/component/header-user.blade.php
@@ -15,13 +15,18 @@
         <li><a href="{{ route('kelas.materi') }}" class="{{ request()->routeIs('kelas.materi') ? 'active' : '' }}">Materi</a></li>
         <li class="dropdown">
           <a href="#">
-            @if(isset($user) && $user->image)
+            <div class="row">
+        <div class="col">
+             @if(isset($user) && $user->image)
             <img class="img-profile rounded-circle" src="{{ asset('storage/profile_images/' . $user->image) }}" width="40" height="40">
             @else
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="35" class="position-absolute top-50 start-50 translate-middle">
               <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
             @endif
+          </div>
+        
+        </div>
             <ul>
               <li><a href="{{ route('profile') }}" class="{{ request()->routeIs('profile') ? 'active' : '' }}">Profil</a></li>
               <li><a href="{{ url('logout') }}">Logout</a></li>
